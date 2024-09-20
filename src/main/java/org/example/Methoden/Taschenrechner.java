2eee11d94163cc5936d6ee8684f6a48c3e24a575
ReReRl1 <roarnga@gmail.com>
2024-09-20 16:15:22 +0200

    Initial Commit

---
+++ src/main/java/org/example/Methoden/Taschenrechner.java
@@ -0,0 +1,17 @@
+package org.example.Methoden;
+/*
+ * Aufgabe: "Taschenrechner"
+ * Schreibe ein Java-Programm, das:
+ * Die vier Grundrechenarten (+, -, *, /) in Methoden auslagert.
+ * Die Methoden sollen zwei Parameter entgegennehmen und das Ergebnis zurückgeben.
+ * Das Programm soll die Methoden aufrufen und die Ergebnisse auf der Konsole ausgeben.
+ * Das Programm soll den Benutzer fragen, ob er eine weitere Berechnung durchführen möchte.
+ */
+public class Taschenrechner {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
