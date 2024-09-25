d1aaec5d7010b265869bed87b7ee0bea6c5f6a0f
ReReRl1 <roarnga@gmail.com>
2024-09-25 16:59:33 +0200

    test

---
+++ src/main/java/org/example/ArrayList/Uebungen/Sortierung.java
@@ -0,0 +1,17 @@
+package org.example.ArrayList.Uebungen;
+
+/*
+ * Aufgabe: "Zahlen zur Liste hinzufügen und sortieren"
+ * Schreibe ein Java-Programm, das:
+ * Eine ArrayList von Integer erstellt mit den Zahlen: { 25, 89, 12, 45, 78, 34, 56, 90, 23, 67 }
+ * Die Liste aufsteigend sortiert und in der Konsole ausgibt.
+ *
+ * Zusatz-1: Der Benutzer soll Zahlen vor der Sortierung hinzufügen können.
+ * Zusatz-2: Der Benutzer soll entscheiden können, ob die Liste aufsteigend oder absteigend sortiert werden soll.
+ * Zusatz-3: Der Benutzer soll nach Ausgabe der Liste Zahlen entfernen können.
+ */
+
+public class Sortierung {
+
+
+}
