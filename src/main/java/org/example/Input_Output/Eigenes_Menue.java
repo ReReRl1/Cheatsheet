f5034ce9f2e594cc709001f732e9595bdd5ac7a7
ReReRl1 <roarnga@gmail.com>
2024-09-25 09:23:01 +0200

    Fixed Method auswahlG and finished project Eigenes_Menue

--- src/main/java/org/example/Input_Output/Eigenes_Menue.java
+++ src/main/java/org/example/Input_Output/Eigenes_Menue.java
@@ -1,62 +1,46 @@
 package org.example.Input_Output;
 
-import javax.smartcardio.TerminalFactory;
 import java.util.Scanner;
 
 public class Eigenes_Menue {
 
-
     public static void main(String[] args) {
-
+        Scanner userEingabe = new Scanner(System.in);
 
         System.out.println("Wie lautet ihr Name?");
-        String name = System.console().readLine();
+        String name = userEingabe.nextLine();  // Use Scanner for input
 
+        String geschlecht = auswahlG(userEingabe);  // Get gender from method
 
-        String geschlecht;
-        Scanner userEingabe = new Scanner(System.in); // auch ueber Scanner moeglich
-
-        System.out.println("Bitte geben Sie ihr Geschlecht an");
-        System.out.println("M | W | D");
-        String auswahl = userEingabe.next();
-
-
-        //name = userEingabe.next();
         System.out.println("Hallo " + geschlecht + name);
-
 
         System.out.println("Gib mir eine Zahl");
         int zahl = userEingabe.nextInt();
+        System.out.println("Die eingegebene Zahl ist: " + zahl);
 
-        System.out.println(zahl);
+        userEingabe.close();  // Close the scanner to prevent resource leaks
+    }
 
+    public static String auswahlG(Scanner scanner) {
+        String geschlecht = "";
+        System.out.println("Bitte geben Sie ihr Geschlecht an");
+        System.out.println("M | W | D");
+        String auswahl = scanner.next();
 
-
-        public String auswahlG() {
-
-            switch (auswahl) {
-                case "M":
-
-                    geschlecht = "Herr ";
-
-                case "W":
-
-                    geschlecht = "Weiblich ";
-
-                case "D":
-
-                    geschlecht = "";
-
-                default:
-
-                    System.out.println("Keine Valide eingabe");
-
-            //ka
-
-            }
-
+        switch (auswahl) {
+            case "M":
+                geschlecht = "Herr ";
+                break;  // Prevent fall-through
+            case "W":
+                geschlecht = "Frau ";
+                break;  // Prevent fall-through
+            case "D":
+                geschlecht = "Divers ";
+                break;  // Prevent fall-through
+            default:
+                System.out.println("Keine valide Eingabe");
+                break;
         }
-
-
+        return geschlecht;  // Return the gender
     }
 }
